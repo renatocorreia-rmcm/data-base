ec921416ccbeda6a52e89de63a9e981c2212150b
renatocorreia-rmcm <rmcm@cin.ufpe.br>
2025-07-20 16:00:16 -0300

    finished table creation example

---
+++ 0_table_creation.py
@@ -0,0 +1,29 @@
+import sqlite3
+
+# conecta ao banco de dados (cria um se ainda não existe)
+connection = sqlite3.connect('clientes.db')
+
+# cursor: iterador que permite navegar e manipular os registros do bd
+cursor = connection.cursor()
+
+
+# execute: lê e executa comandos sql puro diretamente no bd
+cursor.execute(
+	"""
+	CREATE TABLE clientes (
+		id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+		nome TEXT NOT NULL,
+		idade INTEGER,
+		cpf VARCHAR(11) NOT NULL,
+		email TEXT NOT NULL,
+		fone TEXT,
+		cidade TEXT,
+		uf VARCHAR(2) NOT NULL,
+		criado_em DATE NOT NULL
+	);
+	"""
+)
+
+print("table clientes criada.")
+
+connection.close()  # encerra conexão com banco de dados
